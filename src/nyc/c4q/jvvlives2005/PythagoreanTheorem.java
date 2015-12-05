7d4e77df26a7bb575a05af96ebe8c8793188d5a9
jvvlives2005 <jvvlives2005@hotmail.com>
2015-12-05 12:30:14 -0500

    Completed Problem #1

--- src/nyc/c4q/jvvlives2005/PythagoreanTheorem.java
+++ src/nyc/c4q/jvvlives2005/PythagoreanTheorem.java
@@ -1,4 +1,6 @@
 package nyc.c4q.jvvlives2005;
+import java.util.Scanner;
+
 /**
  * Created by Joshelyn on 12/5/15.
 Problem #1: The Pythagorean theorem for the relationship of the lengths of the three sides of a right triangle:
@@ -11,4 +13,24 @@ Problem #1: The Pythagorean theorem for the relationship of the lengths of the t
  */
 public class PythagoreanTheorem
 {
+
+  public static void main (String[] args){
+
+   Scanner input = new Scanner(System.in);
+
+   System.out.println("Enter values to compute the Pythagorean theorem");
+   System.out.print("A: " );
+   double adjacent = input.nextDouble();
+   System.out.print("B: ");
+   double opposite = input.nextDouble() ;
+   System.out.println("Hypotenuse: " + pythagorean(opposite, adjacent));
+   //   System.out.println(pythagorean(getDoubleInput(2.5),getDoubleInput(4.5)));
+  }
+
+  public static double pythagorean(double opposite, double adjacent){
+   double hypothenuse = Math.sqrt(Math.pow(opposite, 2)+ Math.pow(adjacent, 2));
+   return hypothenuse;
+  }
+
+
 }
